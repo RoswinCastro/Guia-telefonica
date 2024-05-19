e4e2994ad32aa1438243787557d06884654048a2
RoswinCastro <roswinjcastror@gmail.com>
2024-05-19 19:08:51 -0400

    Update guia_telefonica_final.sql

--- guia_telefonica_final.sql
+++ guia_telefonica_final.sql
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `guia_telefonica_final`
 --
-CREATE DATABASE  guia_telefonica_final;
+CREATE DATABASE IF NOT EXISTS guia_telefonica_final;
 -- --------------------------------------------------------
 
 --
